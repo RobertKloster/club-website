106169f0c955787051dacb98c27422fcd0095425
CivBase <blange@iastate.edu>
2015-09-19 21:03:19 -0500

    Updated the navbar component

--- static/dist/js/navbar.jsx
+++ static/dist/js/navbar.jsx
@@ -1,26 +1,90 @@
 var NavBar = React.createClass({
+    leftPages: {
+        'Home': 'index.html',
+        'About': 'about/about.html',
+        'Portfolio': 'portfolio/portfolio.html',
+        'Members': 'members/members.html',
+        'Resources': 'resources/resources.html'
+    },
+
+    rightPages: {
+        'Contact Us': 'contact/contact.html'
+    },
+
+    propTypes: {
+        active: React.PropTypes.string,
+        nested: React.PropTypes.number
+    },
+
+    getLinks: function(pages, pathPrefix) {
+        var links = [];
+        for (var page in pages) {
+            if (!pages.hasOwnProperty(page)) {
+                continue;
+            }
+
+            if (this.props.active === page) {
+                links.push((
+                    <li className="active">
+                        <a href="#">
+                            {page}
+                            <span className="sr-only">(current)</span>
+                        </a>
+                    </li>
+                ));
+                continue;
+            }
+
+            links.push((
+                <li>
+                    <a href={pathPrefix + pages[page]}>{page}</a>
+                </li>
+            ));
+        }
+
+        return links;
+    },
+
+    getDefaultProps: function() {
+        return {
+            active: '',
+            nested: 0
+        };
+    },
+
     render: function() {
+        var pathPrefix = '';
+        for (var i = 0; i < this.props.nested; i++) {
+            pathPrefix += '../';
+        }
+
+        var leftLinks = this.getLinks(this.leftPages, pathPrefix);
+        var rightLinks = this.getLinks(this.rightPages, pathPrefix);
+
         return (
             <nav className="navbar navbar-default">
                 <div className="container-fluid">
-                    <ul className="nav navbar-nav">
-                        <li>
-                            <img src="http://placecage.com/100/100" className="img-circle" />
-                        </li>
-                        <li><a href="../index.html">Home</a></li>
-                        <li><a href="../about/about.html">About</a></li>
-                        <li><a href="../portfolio/portfolio.html">Portfolio</a></li>
-                        <li><a href="../members/members.html">Members</a></li>
-                        <li><a href="../resources/resources.html">Resources</a></li>
-                        <li><a href="../contact/contact.html">Contact Us</a></li>
-                    </ul>
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#header-navbar-collapse" aria-expanded="false">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar" />
+                            <span className="icon-bar" />
+                            <span className="icon-bar" />
+                        </button>
+                        <a id="corner-logo-shadow" className="navbar-brand" href="#">
+                            <img id="corner-logo" src={pathPrefix + '_images/WDC-logo.png'} alt="Web Development Club Logo" />
+                        </a>
+                    </div>
+                    <div className="collapse navbar-collapse" id="header-navbar-collapse" style={{marginLeft: 120}}>
+                        <ul className="nav navbar-nav">
+                            {leftLinks}
+                        </ul>
+                        <ul className="nav navbar-nav navbar-right">
+                            {rightLinks}
+                        </ul>
+                    </div>
                 </div>
             </nav>
         );
     }
 });
-
-React.render(
-    <NavBar />,
-    document.getElementById('navbar')
-);
